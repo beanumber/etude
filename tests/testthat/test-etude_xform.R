d1059b0dff1b27af73823b7a8c524a0abb37109d
beanumber <ben.baumer@gmail.com>
2021-03-15 11:34:34 -0400

    added test

--- tests/testthat/test-etude_xform.R
+++ tests/testthat/test-etude_xform.R
@@ -1,3 +1,9 @@
 test_that("etude works", {
-  expect_equal(2 * 2, 4)
+
+  expect_warning(
+    x <- knitr::knit(test_path("etude_test.Rmd"), output = tempfile())
+  )
+  expect_true(file.exists(x))
+  expect_type(x, "character")
+
 })
